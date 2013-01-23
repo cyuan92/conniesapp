c41918cd7eda9570a9c23a0d4b0c6f4597724cda
Connie Yuan <conyuan@ubuntu.(none)>
2013-01-22 17:57:15 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conniesapp::Application.config.secret_token = '61b136b410fbee152929629ee6ad9a40890e24d8305816b850f3ac03cee1b370d6965ee998c0561d642b5c89e808a0ff7c7e6b188b1cdca9dfe1a364b7f1af55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Conniesapp::Application.config.session_store :cookie_store, key: '_conniesapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Conniesapp::Application.config.session_store :active_record_store
