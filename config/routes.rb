d0164bda8b9006386973c54a029de33d1a5cd63f
Connie Yuan <conyuan@ubuntu.(none)>
2013-02-06 10:20:24 -0800

    hw 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Conniesapp::Application.routes.draw do
+  resources :comments
+
+
+  resources :posts
+
+  root :to => 'posts#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
